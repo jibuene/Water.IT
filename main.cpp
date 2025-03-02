133330214cee6ea552e36d653339374a3b39037a
jonbuene <jon.buene@eviny.noclear>
2025-03-02 17:38:02 +0100

    Actual humidity values sending to webserver

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 #include "lwip/apps/http_client.h"
+#include <bits/stdc++.h>
+#include <hardware/adc.h>
 
 #define SENSOR_PIN 26
+
+using namespace std;
 
 
 void connect_to_wifi()
@@ -45,6 +49,33 @@ err_t body(void *arg, struct altcp_pcb *conn,
     return ERR_OK;
 }
 
+void blink_led() {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(500);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(500);
+}
+
+float read_humidity() {
+
+    adc_init();
+
+    adc_gpio_init(26);
+    // Select ADC input 0 (GPIO26)
+    adc_select_input(0);
+
+    const float conversion_factor = 3.3f / (1 << 12);
+    uint16_t result = adc_read();
+
+    float humidity = result * conversion_factor;
+
+    return humidity;
+}
+
+uint16_t convert_to_uint16(float value) {
+    uint16_t converted_value = static_cast<uint16_t>(value * 10000);
+    return converted_value;
+}
 
 int main()
 {
@@ -57,18 +88,24 @@ int main()
     settings.result_fn = NULL;
     settings.headers_done_fn = headers;
     while (true){
+        float humidity = read_humidity();
+
+        // A value of ~ 10000 is a fully saturated plant have I found.
+        // Need to figure out what the humidity for a dry plant is.
+        uint16_t humidity_uint16 = convert_to_uint16(humidity);
+
+        string humidity_str = to_string(humidity_uint16);
+        string url = "/?humidity=" + humidity_str;
         err_t err = httpc_get_file(
                 &ip,
                 8080,
-                "/?name=PicoW",
+                url.c_str(),
                 &settings,
                 body,
                 NULL,
                 NULL); 
 
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(1000);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(1000);
+        blink_led();
+
     }
 }
